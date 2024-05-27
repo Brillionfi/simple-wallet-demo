e4f3d6a99a4ec431f83e1a169a2f7918a0ebaac0
Astrak <coffre.a@gmail.com>
2024-05-27 17:47:03 +0800

    Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home
-      <a href="users/login">Login</a>
+    <main className="flex min-h-screen flex-col items-center justify-around p-24">
+      <div className="flex justify-center flex-col items-center gap-3">
+        <h1>CMS Login</h1>
+        <Button>Log in with Google</Button>
+      </div>
+      <div className="flex justify-center flex-col items-center gap-3">
+        <h1>Wallet Login</h1>
+        <Button>Log in with Google</Button>
+      </div>
     </main>
   );
 }
