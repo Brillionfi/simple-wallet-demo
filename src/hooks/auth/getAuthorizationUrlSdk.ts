23674fa022f8cb96a8fcf9ab4b93c27399a40236
Astrak <coffre.a@gmail.com>
2024-07-25 20:59:39 +0800

    fix(urls): Fix urls

--- src/hooks/auth/getAuthorizationUrlSdk.ts
+++ src/hooks/auth/getAuthorizationUrlSdk.ts
@@ -2,9 +2,9 @@ import { B2B_API_URL, BASE_URL } from "@/utils/constants";
 import { AuthProvider, WalletInfra } from "@brillionfi/wallet-infra-sdk";
 
 export function getAuthorizationUrlSdk(appId: string) {
-  const walletInfra = new WalletInfra(appId, BASE_URL);
+  const walletInfra = new WalletInfra(appId, B2B_API_URL);
   const url = walletInfra.generateAuthUrl(
-    `${B2B_API_URL}/home`,
+    `${BASE_URL}/home`,
     AuthProvider.GOOGLE
   );
   console.log(url);
