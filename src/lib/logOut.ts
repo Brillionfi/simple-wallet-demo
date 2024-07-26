6841a3156510569045f9343586d277442e1de744
Astrak <coffre.a@gmail.com>
2024-07-26 20:37:08 +0800

    fix wrong url

--- src/lib/logOut.ts
+++ src/lib/logOut.ts
@@ -1,8 +1,8 @@
-import { BASE_URL } from "@/utils/constants";
+import { B2B_API_URL, BASE_URL } from "@/utils/constants";
 
 export async function logOut(jwt: string) {
   document.cookie = "session-wallet=";
-  await fetch(`${BASE_URL}/users/logout`, {
+  await fetch(`${B2B_API_URL}/users/logout`, {
     method: "GET",
     headers: {
       Authorization: `Bearer ${jwt}`,
