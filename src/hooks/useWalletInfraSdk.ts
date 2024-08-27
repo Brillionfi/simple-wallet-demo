e35f18d5335d6413c4a70ebeea85abdccb311c09
Santiago Martinez <79987759+santiago-ab@users.noreply.github.com>
2024-08-27 07:24:33 -0400

    fix(): wallet creation error (#20)

--- src/hooks/useWalletInfraSdk.ts
+++ src/hooks/useWalletInfraSdk.ts
@@ -1,21 +1,21 @@
-import { ChainId, WalletInfra } from "@brillionfi/wallet-infra-sdk";
-import { useWalletInfra } from "@/contexts/WalletInfraContext";
-import { createWalletSdk } from "./wallet/createWallet";
-import { signTransactionSdk } from "./wallet/signTransaction";
-import { WalletFormats, WalletTypes } from "@brillionfi/wallet-infra-sdk/dist/models/wallet.models";
-import { getWalletsSdk } from "./wallet/getWallets";
-import { getNotificationsSdk } from "./wallet/getNotifications";
-import { ApproveTransactionSdk, createTransactionSdk, RejectTransactionSdk } from "./transaction/createTransaction";
-import { getPortfolioSdk } from "./portfolio/getPortfolio";
-import { HOSTNAME } from "@/utils/constants";
+import { ChainId, WalletInfra } from '@brillionfi/wallet-infra-sdk';
+import { useWalletInfra } from '@/contexts/WalletInfraContext';
+import { createWalletSdk } from './wallet/createWallet';
+import { signTransactionSdk } from './wallet/signTransaction';
+import { WalletFormats, WalletTypes } from '@brillionfi/wallet-infra-sdk/dist/models/wallet.models';
+import { getWalletsSdk } from './wallet/getWallets';
+import { getNotificationsSdk } from './wallet/getNotifications';
+import { ApproveTransactionSdk, createTransactionSdk, RejectTransactionSdk } from './transaction/createTransaction';
+import { getPortfolioSdk } from './portfolio/getPortfolio';
+import { HOSTNAME } from '@/utils/constants';
 
-export const useWalletInfraSdk = () => {
+export const useWalletInfraSdk = (onError?: (message: string) => void) => {
   const walletInfra = useWalletInfra() as WalletInfra;
   const FromOrigin = HOSTNAME; // or window.location.hostname;
 
   return {
     createWalletSdk: (walletName: string, walletFormat: WalletFormats) =>
-      createWalletSdk(walletInfra, walletName, walletFormat),
+      createWalletSdk(walletInfra, walletName, walletFormat, HOSTNAME, onError),
 
     getWalletsSdk: () => getWalletsSdk(walletInfra),
     getWalletPortfolioSdk: (address: string, chainId: ChainId) => getPortfolioSdk(walletInfra, address, chainId),
@@ -30,45 +30,14 @@ export const useWalletInfraSdk = () => {
       walletType: WalletTypes,
       walletFormat: WalletFormats,
       unsignedTransaction: string,
-    ) => 
-      signTransactionSdk(
-        walletInfra,
-        FromOrigin,
-        address,
-        walletType,
-        walletFormat,
-        unsignedTransaction
-      ),
+    ) => signTransactionSdk(walletInfra, FromOrigin, address, walletType, walletFormat, unsignedTransaction),
 
-    getNotifications:() => 
-      getNotificationsSdk(
-        walletInfra,
-      ),
+    getNotifications: () => getNotificationsSdk(walletInfra),
 
-    approveTransaction:(
-      address: string,
-      organizationId: string,
-      fingerprint: string,
-    ) => 
-      ApproveTransactionSdk(
-        walletInfra,
-        address,
-        organizationId,
-        fingerprint,
-        FromOrigin,
-      ),
+    approveTransaction: (address: string, organizationId: string, fingerprint: string) =>
+      ApproveTransactionSdk(walletInfra, address, organizationId, fingerprint, FromOrigin),
 
-    rejectTransaction:(
-      address: string,
-      organizationId: string,
-      fingerprint: string,
-    ) => 
-      RejectTransactionSdk(
-        walletInfra,
-        address,
-        organizationId,
-        fingerprint,
-        FromOrigin,
-      ),
+    rejectTransaction: (address: string, organizationId: string, fingerprint: string) =>
+      RejectTransactionSdk(walletInfra, address, organizationId, fingerprint, FromOrigin),
   };
 };
