7eed7b041930efd561c115e688422a5e4a931754
santiago-ab <santiago.martinez@brillion.finance>
2024-11-25 12:13:26 -0400

    fix(): url request params

--- src/hooks/auth/getAuthorizationUrlSdk.ts
+++ src/hooks/auth/getAuthorizationUrlSdk.ts
@@ -1,11 +1,15 @@
 import { AuthProvider, WalletInfra } from '@brillionfi/wallet-infra-sdk';
+import { IAuthURLParams } from '@brillionfi/wallet-infra-sdk/dist/models';
 
 export function getAuthorizationUrlSdk(provider: AuthProvider, appId: string, email?: string) {
   const walletInfra = new WalletInfra(appId, process.env.NEXT_PUBLIC_API_URL as string);
-  const url = walletInfra.generateAuthUrl({
+
+  const params: IAuthURLParams = {
     provider,
-    redirectUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/home`,
-    email,
-  });
+    redirectUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/home`
+  }
+  if(email) params.email = email;
+
+  const url = walletInfra.generateAuthUrl(params);
   return url;
 }
