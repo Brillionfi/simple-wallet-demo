82ae1849acebc62b4826851c2f70e161dc85f806
santiago-ab <santiago.martinez@brillion.finance>
2024-12-16 19:28:50 -0400

    feat(): login form in modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,12 @@ import {useLoginIfSession} from "@/lib/loginIfSession";
 import {useEffect, useState} from "react";
 import { useSearchParams } from "next/navigation";
 import { BrillionProvider, LoginForm, LoginMethods, defaultStyles } from '@brillionfi/waas-react-sdk';
+import { Modal, Sheet } from "@mui/joy";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const [appId, setAppId] = useState<string>("");
+  const [open, setOpen] = useState<boolean>(false);
   const searchParams = useSearchParams();
 
   useEffect(() => {
@@ -18,25 +21,39 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center gap-6 p-24">
       <BrillionProvider appId={appId} baseUrl={process.env.NEXT_PUBLIC_API_URL as string} WCProjectId={process.env.NEXT_PUBLIC_WC_PROJECT_ID as string}>
-        <LoginForm 
-          loginMethods={[
-            LoginMethods.Google, 
-            LoginMethods.Discord, 
-            LoginMethods.Twitter, 
-            LoginMethods.Metamask, 
-            LoginMethods.WalletConnect, 
-            LoginMethods.Email
-          ]} 
-          redirectUrl={process.env.NEXT_PUBLIC_REDIRECT_URL as string}
-          customProps={{
-            containerStyle: {
-              ...defaultStyles.container,
-            },
-            tittleStyle: {
-              ...defaultStyles.tittle,
-            },
-          }}
-        />
+        <Button 
+          onClick={()=>setOpen(true)}
+        >
+          Connect Wallet
+        </Button>
+        <Modal
+          aria-labelledby="modal-title"
+          aria-describedby="modal-desc"
+          open={open}
+          onClose={()=>setOpen(false)}
+          sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+        >
+          <LoginForm 
+            loginMethods={[
+              LoginMethods.Google, 
+              LoginMethods.Discord, 
+              LoginMethods.Twitter, 
+              LoginMethods.Metamask, 
+              LoginMethods.WalletConnect, 
+              LoginMethods.Email
+            ]} 
+            onClose={()=>setOpen(false)}
+            redirectUrl={process.env.NEXT_PUBLIC_REDIRECT_URL as string}
+            customStyles={{
+              containerStyle: {
+                ...defaultStyles.container,
+              },
+              headerStyle: {
+                ...defaultStyles.header,
+              },
+            }}
+          />
+        </Modal>
       </BrillionProvider>
     </main>
   );
