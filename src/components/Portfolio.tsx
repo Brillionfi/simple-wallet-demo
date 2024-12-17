8ff0a39e09d76f9ee643f70ad45215528895f5e4
Santiago Martinez <79987759+santiago-ab@users.noreply.github.com>
2024-12-17 10:41:36 -0400

    Feat/new components (#31)
    * feat(): new sdk version
    
    * fix(): codacy
    
    * feat(): chain selector and portfolio table
    
    * feat(): update sdk version
    
    * feat(): login form in modal
    
    * fix(): sdk version
    
    * fix(): codacy

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,56 +1,21 @@
-import { getChainsForFormat } from "@/utils/getChainsForFormat";
-import React, { useEffect, useMemo, useState } from "react";
-import { WalletFormats, WalletTypes } from "@brillionfi/wallet-infra-sdk/dist/models/wallet.models";
-import { Assets, PortfolioTable } from "./Portfolio/PortfolioTable";
-import CircularProgress from '@mui/material/CircularProgress';
-import { useBalance } from "@brillionfi/waas-react-sdk";
+import { AssetsTable, useBrillionContext } from "@brillionfi/waas-react-sdk";
 
 export function Portfolio({
   account,
-  format,
-  walletType,
 }: {
   account: string;
-  format: WalletFormats;
-  walletType: WalletTypes;
 }) {
-  const chains = getChainsForFormat(format);
-  const { getPortfolio }= useBalance();
-  const [portfolio, setPortfolio] = useState<Assets[]>();
-
-  useEffect(() => {
-    const getData = async () => {
-      const promises = [];
-      if(account && chains){
-        for (const chain of chains) {
-          promises.push(getPortfolio(account, chain));
-        }
-      }
-      const results = await Promise.all(promises);
-      const portfolios:Assets[] = [];
-      results.forEach(result => {
-        result?.portfolio.forEach(element => {
-          portfolios.push({...element, chainId: result.chainId});
-        });
-      }); 
-      if(portfolios.length > 0) setPortfolio(portfolios);
-    }
-    void getData();
-  }, [])
+  const { chain } = useBrillionContext();
 
   return (
     <div className="flex gap-5 flex-col w-full">
       <div className="flex w-full justify-between items-end">
         <div>
           <h2 className="inline">Portfolio</h2>
-          <i className="text-xs"> - ({account})</i>
+          <i className="text-xs"> - ({account}) - ChainId: {chain}</i>
         </div>
       </div>
-      {portfolio ? 
-        <PortfolioTable assets={portfolio} account={account} format={format} walletType={walletType} />
-      :
-        <CircularProgress size={15}/>
-      }
+      <AssetsTable address={account}/>
     </div>
   );
 }
