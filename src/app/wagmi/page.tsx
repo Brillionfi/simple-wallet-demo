1d8267cc4e2397253421ef8f6fe555612979c40a
santiago-ab <santiago.martinez@brillion.finance>
2025-01-29 18:28:04 -0400

    feat(): first wagmi connector implementation

---
+++ src/app/wagmi/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import HomePageWagmi from "@/components/HomePageWagmi";
+import { jwtDecode } from "@/utils/jwt-decode";
+import { brillionWagmi } from "@brillionfi/waas-react-sdk";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { createConfig, WagmiProvider } from 'wagmi';
+import { mainnet, polygon, sepolia, polygonAmoy } from 'wagmi/chains'
+
+const queryClient = new QueryClient();
+
+const { brillionConnector, brillionTransport } = brillionWagmi({
+  appId: process.env.NEXT_PUBLIC_DEFAULT_APPID ?? "7cd8e911-cb89-4bdf-9fa0-d5bb9563158b",
+  baseUrl: process.env.NEXT_PUBLIC_API_URL as string,
+  defaultNetwork: polygon.id,
+  WcProjectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID as string
+})
+
+const config = createConfig({
+  chains: [mainnet, polygon, sepolia, polygonAmoy],
+  transports: {
+    [mainnet.id]: brillionTransport(mainnet.id),
+    [polygon.id]: brillionTransport(polygon.id),
+    [sepolia.id]: brillionTransport(sepolia.id),
+    [polygonAmoy.id]: brillionTransport(polygonAmoy.id),
+  },
+  connectors: [brillionConnector]
+})
+
+export default function () {
+  const [appId, setAppId] = useState<string>("");
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const error = searchParams?.get("error");
+    if(error){
+      alert("Error: " + error);
+      window.location.href = "/";
+      return;
+    }
+    
+    const jwt = searchParams?.get("code");
+    if(!jwt) return;
+
+    const info = JSON.parse(jwtDecode(jwt.split(".")[1]));
+    setAppId(info.appId && process.env.NEXT_PUBLIC_DEFAULT_APPID);
+  }, [])
+  
+  return (
+    <QueryClientProvider client={queryClient}>
+      <WagmiProvider config={config}>
+        <HomePageWagmi />
+      </WagmiProvider>
+    </QueryClientProvider>
+  );
+}
---
+++ src/components/HomePageWagmi.tsx
@@ -0,0 +1,230 @@
+import { useAccount, useConnect, useDisconnect, useSendTransaction, useSignMessage, useBalance, useBlockNumber, useGasPrice, useWriteContract } from "wagmi";
+import { ConnectBrillionProps } from "@brillionfi/waas-react-sdk";
+import { Button } from "@/components/ui/button";
+import { ChainSelector } from "@/components/ui/ChainSelector";
+
+export default function WagmiHomePage() {
+  const { connect, connectors } = useConnect();
+  const { isConnected, address, chainId } = useAccount();
+  const { disconnect } = useDisconnect()
+  const { writeContract } = useWriteContract()
+  const { signMessage } = useSignMessage()
+  const blockNumber = useBlockNumber()
+  const gasPrice = useGasPrice()
+  const balance = useBalance({ address })
+  // const transactionCount = useTransactionCount({
+  //   address,
+  // })
+  // const estimate = useEstimateGas({
+  //   account: address, 
+  //   to: '0xd2135CfB216b74109775236E36d4b433F1DF507B',
+  //   value: BigInt(1),
+  // })
+  // const usdtSupply = useReadContract({
+  //   abi: [{
+  //     "inputs": [],
+  //     "name": "totalSupply",
+  //     "outputs": [
+  //       {
+  //         "internalType": "uint256",
+  //         "name": "",
+  //         "type": "uint256"
+  //       }
+  //     ],
+  //     "stateMutability": "view",
+  //     "type": "function"
+  //   }],
+  //   address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+  //   functionName: 'totalSupply',
+  // })
+  // const usdt = useToken({
+  //   address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+  // })
+  // const feeHistory = useFeeHistory({
+  //   blockCount: 4,
+  //   rewardPercentiles: [25, 75]
+  // })
+  // const simulate = useSimulateContract({
+  //   abi: [{
+  //     "inputs": [
+  //       {
+  //         "internalType": "address",
+  //         "name": "from",
+  //         "type": "address"
+  //       },
+  //       {
+  //         "internalType": "address",
+  //         "name": "to",
+  //         "type": "address"
+  //       },
+  //       {
+  //         "internalType": "uint256",
+  //         "name": "amount",
+  //         "type": "uint256"
+  //       }
+  //     ],
+  //     "name": "transferFrom",
+  //     "outputs": [
+  //       {
+  //         "internalType": "bool",
+  //         "name": "",
+  //         "type": "bool"
+  //       }
+  //     ],
+  //     "stateMutability": "nonpayable",
+  //     "type": "function"
+  //   }],
+  //   address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+  //   functionName: 'transferFrom',
+  //   args: [
+  //     address as `0x${string}`,
+  //     '0xA0Cf798816D4b9b9866b5330EEa46a18382f251e',
+  //     BigInt(1),
+  //   ],
+  // })
+
+  const { sendTransaction, data, isSuccess } = useSendTransaction();
+
+  const BrillionConnect = (provider: string, email?: string, walletName?: string) => {
+    connect({
+      connector: connectors[0],
+      ...{
+        provider,
+        redirectUrl: `${process.env.NEXT_PUBLIC_BASE_URL as string}/wagmi`,
+        email,
+        walletName,
+      } as ConnectBrillionProps
+    })
+  }
+
+
+  const disconnectWallet = () => {
+    disconnect();
+  }
+
+  const write = () => {
+    writeContract({ 
+      abi: [{
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "from",
+            "type": "address"
+          },
+          {
+            "internalType": "address",
+            "name": "to",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "amount",
+            "type": "uint256"
+          }
+        ],
+        "name": "transferFrom",
+        "outputs": [
+          {
+            "internalType": "bool",
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      }],
+      address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+      functionName: 'transferFrom',
+      args: [
+        address as `0x${string}`,
+        '0xA0Cf798816D4b9b9866b5330EEa46a18382f251e',
+        BigInt(1),
+      ],
+    })
+  }
+
+  const sendTx = () => {
+    sendTransaction({
+      to: "0x840B04a984b5BCD3aD2C754556f99Db3015dc3Bc",
+      value: BigInt("1"),
+      data: "0x0"
+    });
+  }
+
+  const thStyle = "text-center font-normal border-slate-200 border py-2 px-3";
+  const tdStyle = "px-3 border-slate-200 border";
+  return (
+    <div className="min-h-screen px-10 bg-white">
+      <div className="flex flex-col items-center gap-10 w-[800px] justify-start mt-7">
+        {!isConnected && 
+          <>
+            <Button onClick={()=>BrillionConnect("Google")} >
+              Connect Wallet with Google
+            </Button>
+            <Button onClick={()=>BrillionConnect("Discord")} >
+              Connect Wallet with Discord
+            </Button>
+            <Button onClick={()=>BrillionConnect("WalletConnect")} >
+              Connect Wallet with WalletConnect
+            </Button>
+            <Button onClick={()=>BrillionConnect("Metamask")} >
+              Connect Wallet with Metamask
+            </Button>
+          </>
+        }
+        {isConnected && 
+          <>
+            <div className='flex gap-5'>
+              <ChainSelector />
+              <Button onClick={()=>disconnectWallet} >
+                disconnect Wallet
+              </Button>
+            </div>
+            <hr />
+            <table className="rounded-md overflow-hidden text-gray-500 border-solid border-slate-200 text-sm relative">
+              <thead className="bg-slate-100">
+                <tr>
+                  <th className={thStyle}>Data</th>
+                  <th className={thStyle}>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className={`${tdStyle} w-1/12`}>Address</td>
+                  <td className={`${tdStyle} w-1/12`}>{address}</td>
+                </tr>
+                <tr>
+                  <td className={`${tdStyle} w-1/12`}>Current Chain</td>
+                  <td className={`${tdStyle} w-1/12`}>{chainId}</td>
+                </tr>
+                <tr>
+                  <td className={`${tdStyle} w-1/12`}>Balance</td>
+                  <td className={`${tdStyle} w-1/12`}>{`${String(balance.data?.value)} ${balance.data?.symbol}`}</td>
+                </tr>
+                <tr>
+                  <td className={`${tdStyle} w-1/12`}>Block Number</td>
+                  <td className={`${tdStyle} w-1/12`}>{String(blockNumber.data)}</td>
+                </tr>
+                <tr>
+                  <td className={`${tdStyle} w-1/12`}>Gas Price</td>
+                  <td className={`${tdStyle} w-1/12`}>{String(gasPrice.data)}</td>
+                </tr>
+              </tbody>
+            </table>
+            <div className='flex gap-5'>
+              <Button onClick={sendTx} >
+                Send tx
+              </Button>
+              <Button onClick={write} >
+                Write tx
+              </Button>
+              <Button onClick={()=>signMessage({ message: 'hello world' })} >
+                Sign message
+              </Button>
+            </div>
+          </>
+        }
+      </div>
+    </div>
+  );
+}
